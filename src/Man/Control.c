e1c80124da5cb5309abc56efad2da10e365b8b5b
Matsumotorise <35851996+Matsumotorise@users.noreply.github.com>
2018-04-23 16:27:49 -0400

    Add files via upload

--- src/Man/Control.c
+++ src/Man/Control.c
@@ -1,5 +1,8 @@
-#pragma config(UserModel, "C:/Users/Andrew/Documents/Programing/RobotC/Projects/Testing/src/plainClawBot.h")
-#include "C:/Users/Andrew/Documents/Programing/RobotC/Projects/Testing/src/Man/Headers/Control.h"
+#pragma config(UserModel, "C:/Users/staff/Desktop/clawBot-master/src/plainClawBot.h")
+
+
+#include "C:/Users/staff/Desktop/clawBot-master/src/Man/Headers/Control.h"
+
 
 task main(){
 	//Check in separate threads
